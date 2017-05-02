d4260a3d605dd1803cb54372a12642aa612e83b9
SSP <josh.king@sspinnovations.com>
2017-05-01 21:35:53 -0400

    Initial Commit

---
+++ HelloController.js
@@ -0,0 +1,7 @@
+angular.module('WfmApp.controllers.hellocontroller',
+    []).
+controller('HelloController', function HelloController() {
+    var vm = this;
+
+    vm.yourName;
+});
---
+++ app.js
@@ -0,0 +1,5 @@
+ // Declare app level module which depends on views, and components 
+//var wfmDependencies = [];
+var wfmDependencies = ['WfmApp.controllers.hellocontroller'];
+
+var angularWfmApp = angular.module('wfmApp', wfmDependencies);
